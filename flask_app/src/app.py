ab5b5c012492f8bce39a02ded4ef73746eb30a88
jcdd1 <cami-516@hotmail.com>
2025-10-23 08:31:44 -0500

    inittial commit

---
+++ flask_app/src/app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Mi primer hola mundo'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    username = request.form["username"]
+    password = request.form["chinga tu madre"]
+    if usernmame == username and chinga tu madre ==
+PASSWORD: 
+    pylance: return "felicidades, entraste y no sirvió para nada!!!!"
+    else:
+        return: "anda a meter las cosas incorrectas en otra parte", 401
+
+if __name__ == "__main__":
+      app.run(debug=True)
+
+
+
