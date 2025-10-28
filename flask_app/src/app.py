9d20b2c0ebefe4b0d7e27e9ec7cd5e4512cb7018
jcdd1 <cami-516@hotmail.com>
2025-10-28 08:07:03 -0500

    feat: add Dockerfile

--- flask_app/src/app.py
+++ flask_app/src/app.py
@@ -1,24 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def ruta():
     return 'Mi primer hola mundo'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    username = request.form["username"]
-    password = request.form["chinga tu madre"]
-    if usernmame == username and chinga tu madre ==
-PASSWORD: 
-    pylance: return "felicidades, entraste y no sirvió para nada!!!!"
-    else:
-        return: "anda a meter las cosas incorrectas en otra parte", 401
 
-if __name__ == "__main__":
-      app.run(debug=True)
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
 
