bd05a8c82671e0dfc4808d75490c332fedef5d39
jcdd1 <cami-516@hotmail.com>
2025-10-30 18:20:35 -0500

    fix: change route in app.py

--- flask_app/src/app.py
+++ flask_app/src/app.py
@@ -1,43 +1,39 @@
 import os
 from flask import Flask, render_template, jsonify, request
 from flask_pymongo import PyMongo
-# --- NUEVO: Importar python-dotenv ---
+
 from dotenv import load_dotenv
 
-# --- NUEVO: Cargar el archivo .env ---
-# 1. Definimos la ruta al archivo .env (que está en 'proyecto/', un nivel arriba de 'src/')
+
+# Definimos la ruta al archivo .env
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-# 2. Cargamos las variables de ese archivo
+
 load_dotenv(dotenv_path)
-# ------------------------------------
+
 
 app = Flask(__name__)
 
-# --- Configuración de MongoDB ---
-# Lee la variable MONGO_URI (que load_dotenv() acaba de cargar)
 mongo_uri = os.environ.get('MONGO_URI')
 
 if not mongo_uri:
     print("Error: La variable de entorno MONGO_URI no está configurada.")
-    # Opcional: puedes salir o manejar el error como prefieras
+
 
 print(f"Intentando conectar a MongoDB...")
 app.config["MONGO_URI"] = mongo_uri
 
 try:
     mongo = PyMongo(app)
-    # Accedemos a la colección 'sensor1' que mencionaste
+    
     sensor1_collection = mongo.db.sensor1 
     print("Conexión a MongoDB y colección 'sensor1' establecida.")
-    # Prueba de conexión rápida
+
     sensor1_collection.find_one()
     print("Prueba de lectura a la colección 'sensor1' exitosa.")
 except Exception as e:
     print(f"Error al conectar o interactuar con MongoDB: {e}")
     mongo = None
     sensor1_collection = None
-# ------------------------------
-
 
 @app.route('/')
 def ruta():
@@ -48,8 +44,6 @@ def ruta():
 def index():
     return render_template('index.html')
 
-
-# --- RUTA PARA MANDAR DATOS (PRUEBA) ---
 @app.route('/agregar_dato_prueba')
 def agregar_dato_prueba():
     """
@@ -58,7 +52,7 @@ def agregar_dato_prueba():
     """
     if sensor1_collection is not None:
         try:
-            # Creamos un dato de ejemplo
+            
             dato_sensor = {"sensor": "temperatura_prueba", "valor": 30.1, "unidad": "C"}
             # Insertamos el dato en la colección 'sensor1'
             result = sensor1_collection.insert_one(dato_sensor)
